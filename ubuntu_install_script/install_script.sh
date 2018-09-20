b7024a798dc6bdf6d5411f1da251de895f420932
realestninja <the@realest.ninja>
2018-09-20 16:52:57 +0200

    Installscript: Add gimp

--- ubuntu_install_script/install_script.sh
+++ ubuntu_install_script/install_script.sh
@@ -89,6 +89,7 @@ then
 	sudo apt install clementine
 	sudo apt install scrot
 	sudo apt install filezilla
+	sudo apt install gimp
 	sudo apt install xdotool
 	sudo apt install xserver-xorg-input-synaptics
 	sudo apt install fortune
